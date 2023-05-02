6486d61a4d02e72c443691123a9b5d613eb8f02e
Malcolmston <67117350+Malcolmston@users.noreply.github.com>
2023-05-02 12:13:07 -0400

    adds both thing from las 2 merges as well as new popups

--- files/js/colGroup.js
+++ files/js/colGroup.js
@@ -1,3 +1,32 @@
+/**
+  * this function fetes data using a post method. 
+  * @param {String} url the url to fetch from
+  * @param {Object} data is what is being posted 
+  * @returns {Promise<JSON>} onse resolved it will fetch the response data
+  * @see  thanks too https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch for the following code
+  */
+ async function postData(url = "", data = {}) {
+   // Default options are marked with *
+   const response = await fetch(url, {
+     method: "POST", // *GET, POST, PUT, DELETE, etc.
+     mode: "cors", // no-cors, *cors, same-origin
+     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+     credentials: "same-origin", // include, *same-origin, omit
+     headers: {
+       "Content-Type": "application/json",
+       // 'Content-Type': 'application/x-www-form-urlencoded',
+     },
+     redirect: "follow", // manual, *follow, error
+     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+     body: JSON.stringify(data), // body data type must match "Content-Type" header
+   });
+   return response.json(); // parses JSON response into native JavaScript objects
+ }
+
+
+
+
+
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 //https://getbootstrap.com/docs/5.3/forms/validation/
 (() => {
@@ -20,18 +49,12 @@
 })()
 
 const allnameForms = document.querySelectorAll('[action="/aplyName"]')
-
-
-
-
-
-
 const ImageNumber = document.querySelector("[name='ImageNumber'] ") 
 const applyClick = document.querySelector(".applyClick")
 const helpText = document.querySelector(".helpText")
 const nameDP = document.querySelector(".name")
-
-
+const form = document.querySelector('.needs-feedback')
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
 const clickedElements = []
 
 function selected( element ){
@@ -50,9 +73,8 @@ function selected( element ){
   element.classList.add("selected")
 
 
-  helpText.innerHTML = `Yow have selected phot number ${element.id}`
+  helpText.innerHTML = `Yow have selected photo number ${element.id}`
 }
-
 
 nameDP.addEventListener("change",function(){
   allnameForms.forEach(function (form) {
@@ -65,3 +87,58 @@ nameDP.addEventListener("change",function(){
       document.querySelector(`.${this.value}`).classList.replace('hide', 'show')
      
 })
+
+
+
+
+
+
+
+//https://getbootstrap.com/docs/5.3/components/alerts/ for the message popups
+
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = 
+    `<div class="alert alert-${type} alert-dismissible" role="alert">
+      <div>${message}</div>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`
+  
+
+  alertPlaceholder.append(wrapper)
+}
+
+
+form.addEventListener('submit', event => {
+    let items = []
+     // if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      //}
+
+      form.classList.add('was-validated')
+      
+    Array.from(form.elements).forEach(x => {
+          if( x.id == "validUsername" || x.id == "validPassword"){
+            items.push( x.value )
+          }
+          
+      })
+      
+
+
+      postData("/login",{
+          type: "json",
+          
+          username: items[0],
+          password: items[1]
+      }).then(function(ele){
+          if( ele[0].valid  ){
+            appendAlert("you are logged in","success")
+          }else{
+            appendAlert(ele[0].message,"danger")
+          }
+      })
+      
+      
+}, false)
